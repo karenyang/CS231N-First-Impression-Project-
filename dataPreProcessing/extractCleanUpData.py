59c80c0d8edb09aaa352744eaae6d85e308cc037
Noa Glaser <noa.glaser@gmail.com>
2017-05-25 06:02:49 +0000

    resnNetDataStuff

--- dataPreProcessing/extractCleanUpData.py
+++ dataPreProcessing/extractCleanUpData.py
@@ -1,8 +1,9 @@
 import sys 
-import random
+import random # for splitting into train/val
 import os
-import pickle
-
+import pickle # for resetting name 
+from PIL import Image # for data validation 
+ 
 def splitIntoTrainVal(directory, ratio_of_train):
   dirListing = os.listdir(directory)
   random.shuffle(dirListing)
@@ -30,13 +31,30 @@ def resetNamesBack(dr):
     if(label_dicts.get(fileName)== None):
         print fileName, 'not in dictionary'
         fileNameCp  = fileName
-	for i in range(3):
-          fileNameCp  = '-' + fileNameCp
-    	  if(label_dicts.get(fileNameCp) != None):
+    for i in range(3):
+        fileNameCp  = '-' + fileNameCp
+        if(label_dicts.get(fileNameCp) != None):
             print 'renaming', os.path.join(dr, fileName),os.path.join( dr, fileNameCp)
-            # os.rename(os.path.join(dr, fileName),os.path.join( dr, fileNameCp))
-            pass 
+            os.rename(os.path.join(dr, fileName),os.path.join( dr, fileNameCp))
 
+def validateAllData(path):
+    numClean = 0
+    numDirty = 0
+    directories = [d for d in os.listdir('./') if os.path.isdir(d)]
+    for dirName in directories:
+        targetDir = os.path.join(path, dirName)
+        for f in os.listdir(targetDir):
+            fName = os.path.join(targetDir, f)
+            try:
+                im = Image.open(fName).convert('RGB')
+                numClean = numClean + 1
+            except ValueError:
+                print "couldn't open " + fName
+                numDirty = numDirty +1 
+                print "numDirty" , numDirty, "numClean", numClean
+                os.system('rm ' + dirty)
+    print "overall ", numClean, "clean"
+    
 def extractFromZipMove():
   destFile = 'train-2/'
   extractPrefix = 'train80'
@@ -49,8 +67,9 @@ def extractFromZipMove():
     while newFileName.startswith('-'):
       newFileName = newFileName[1:]
     os.system('mv ./' + fileName + ' ' + destFile + newFileName)
-	
+
 
 if __name__ == '__main__': 
     sourceFile = sys.argv[1]
-    resetNamesBack(sourceFile)
+    # resetNamesBack(sourceFile)
+    validateAllData(sourceFile)
