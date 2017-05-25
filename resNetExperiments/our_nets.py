8aa74972af8056872bbd33b3855ac6590822b373
root <root@with-gpu.c.first-impression-project.internal>
2017-05-25 06:01:08 +0000

    create resnet_for_rnn

--- resNetExperiments/our_nets.py
+++ resNetExperiments/our_nets.py
@@ -33,22 +33,32 @@ class ResNet50_Noa_Karen(nn.Module):
         x = self.sigmoid(x)
         return x
 
-# Adopted from: https://github.com/pytorch/vision/blob/master/torchvision/models/densenet.py
-# TODO: check whether this is usable and within memory limits
-class DenseNet161_Noa_Karen(nn.Module):
-    def __init__(self,model_pretained=None,num_classes=5):
-        super(DenseNet161_Noa_Karen, self).__init__()      
-        self.densenet = model_pretained
-        self.sigmoid = nn.Sigmoid()           
-    def forward(self, x):
-        # TODO
-        #x = self.densenet.features(x) # everything before the final classifier layer
-        #x = x.view(x.size(0), -1)
+
+class ResNet50_Noa_Karen_128to5(nn.Module):
+    def __init__(self,model_pretained=None,hidden_size=128,num_classes=5):
+        super(ResNet50_Noa_Karen_128to5, self).__init__()
         
-        #x = self.new_fc(x)
+        self.resnet = model_pretained
+        self.fc_128to5 = nn.Linear(hidden_size,num_classes)
+        self.sigmoid = nn.Sigmoid()   
+        
+    def forward(self, x):
+        x = self.resnet.conv1(x)
+        x = self.resnet.bn1(x)
+        x = self.resnet.relu(x)
+        x = self.resnet.maxpool(x)
+        x = self.resnet.layer1(x)
+        x = self.resnet.layer2(x)
+        x = self.resnet.layer3(x)
+        x = self.resnet.layer4(x)
+        x = self.resnet.avgpool(x) # everything before the final fc layer       
+        x = x.view(x.size(0), -1)
+        x = self.resnet.fc(x)
+        x = self.fc_128to5(x)
         x = self.sigmoid(x)
         return x
-
+    
+    
 def resnet50_noa_karen(num_classes = 5,verbose=False):
     model_pretained = torchvision.models.resnet50(pretrained=True)
     # All of the parameters are freezed, not to change (newly constructed layers' params won't be influenced)
@@ -61,13 +71,16 @@ def resnet50_noa_karen(num_classes = 5,verbose=False):
         print(model_base.modules)
     return model_base
 
-def densenet161_noa_karen(num_classes = 5, verbose=False):
-    model_pretained = torchvision.models.densenet161(pretrained=True)
+def resnet50_noa_karen_128to5(hidden_size = 128, num_classes = 5,verbose=False):
+    model_pretained = torchvision.models.resnet50(pretrained=True)
     # All of the parameters are freezed, not to change (newly constructed layers' params won't be influenced)
     for param in model_pretained.parameters():
-        param.requires_grad = False    
-    model_pretained.classifier = nn.Linear(model_pretained.classifier.in_features, num_classes)
-    model_base = DenseNet161_Noa_Karen(model_pretained=model_pretained,num_classes=num_classes)
+        param.requires_grad = False   
+        
+    model_pretained.fc = nn.Linear(model_pretained.fc.in_features, hidden_size)
+    model_base = ResNet50_Noa_Karen_128to5(model_pretained=model_pretained,hidden_size =hidden_size, num_classes=num_classes)
     if verbose:
         print(model_base.modules)
     return model_base
+
+
